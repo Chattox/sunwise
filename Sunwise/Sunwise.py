afca36a7f8bfec205f28ffd0a2e0269b278619bb
Chattox <conradlineker@gmail.com>
2025-01-16 12:48:46 +0000

    feat(sunwise): add caching readings to file for later upload

---
+++ Sunwise/Sunwise.py
@@ -0,0 +1,33 @@
+from os import makedirs
+from json import dump
+from Sunwise.Logger import Logger
+from Sunwise.Sensors import Sensors
+from utils.datetime_string import datetime_string
+
+class Sunwise():
+    """
+    Handles overall functionality of the Sunwise weather station
+    """
+
+    def __init__(self):
+        self.logger = Logger()
+        self.sensors = Sensors(self.logger)
+
+    def take_readings(self):
+        """
+        Get data from all sensors and cache to readings file for later upload
+        """
+        now = datetime_string()
+        readings = self.sensors.get_readings()
+        cache_payload = {
+            "nickname": "sunwise",
+            "timestamp": now,
+            "readings": readings
+        }
+        uploads_filename = f"uploads/{datetime_string(filename=True)}.json"
+
+        makedirs("uploads", exist_ok=True)
+
+        self.logger.log("info", "Caching readings for later upload")
+        with open(uploads_filename, "w") as upload_file:
+            dump(cache_payload, upload_file, indent=4)
