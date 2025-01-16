82949fe764a566e155d37974aacdbd9db6e0e224
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2025-01-16 12:49:55 +0000

    Feat/save sensor readings to file (#2)
    * feat(utils): add datetime_string util
    
    * feat(sunwise): add caching readings to file for later upload

--- Sunwise/Sensors.py
+++ Sunwise/Sensors.py
@@ -4,14 +4,18 @@ import bme280
 class Sensors():
     """
     Handles config and reading of attached sensors
-    """
-    def __init__(self):
-        self.port = 1
-        self.address = 0x77
-        self.bus = smbus2.SMBus(self.port)
-        self.calibration_params = bme280.load_calibration_params(self.bus, self.address)
 
-    def get_bme280(self):
+    Args:
+        logger (Logger): Logger instance
+    """
+    def __init__(self, logger):
+        self.__logger = logger
+        self.__port = 1
+        self.__address = 0x77
+        self.__bus = smbus2.SMBus(self.__port)
+        self.__calibration_params = bme280.load_calibration_params(self.__bus, self.__address)
+
+    def __get_bme280(self):
         """
         Get temperature, pressure, and humidity readings from the BME280
 
@@ -19,11 +23,33 @@ class Sensors():
             dict: Temperature, humidity, and pressure readings rounded
             to 2 decimal places
         """
-        data = bme280.sample(self.bus, self.address, self.calibration_params)
-        print(data.temperature)
+        data = bme280.sample(self.__bus, self.__address, self.__calibration_params)
         readings_dict = {
-            "temperature": round(data.temperature, 2),
-            "humidity": round(data.humidity, 2),
-            "pressure": round(data.pressure, 2)
+            "temperature": data.temperature,
+            "humidity": data.humidity,
+            "pressure": data.pressure
         }
+        return readings_dict
+    
+    def get_readings(self):
+        """
+        Take readings from all sensors and return a dict containing them
+
+        Returns:
+            dict: sensor readings
+        """
+        self.__logger.log("info", "Taking new readings...")
+
+        bme280_readings = self.__get_bme280()
+        readings_dict = {
+                "temperature": round(bme280_readings["temperature"], 2),
+                "humidity": round(bme280_readings["humidity"], 2),
+                "pressure": round(bme280_readings["pressure"], 2),
+                "luminance": 0,
+                "wind_speed": 0,
+                "rain": 0,
+                "rain_per_second": 0,
+                "wind_direction": 0
+        }
+
         return readings_dict
