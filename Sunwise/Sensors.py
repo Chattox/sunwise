80804981fecf9e6f273cae83f87b800d2702c804
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2025-02-01 21:41:51 +0000

    feat(sensors): add wind speed and direction functionality (#7)
    * feat(sensors): wind speed sensor reading and recording
    
    * refactor: change imports
    
    * feat(sensors): wind direction sensor functionality
    
    * feat(sensors): if no wind, duplicate most recent wind dir on reading
    
    * feat(sensors): add average and gust wind speeds to readings payload
    
    * feat(sensors): add wind direction to readings payload

--- Sunwise/Sensors.py
+++ Sunwise/Sensors.py
@@ -1,11 +1,12 @@
 import smbus2
 import bme280
 import warnings
-from os import remove, path
-from gpiozero import PinFactoryFallback
-from gpiozero import Button
+import math
+import os
+import statistics
+from gpiozero import PinFactoryFallback, Button, MCP3008
 from utils.datetime_string import datetime_string
-from config import RAIN_SENSOR_MM
+from config import RAIN_SENSOR_MM, WIND_RADIUS, WIND_INTERVAL, WIND_ADJUSTMENT, WIND_DIR_VOLTS
 
 
 
@@ -24,6 +25,10 @@ class Sensors():
         self.__bus = smbus2.SMBus(self.__port)
         self.__calibration_params = bme280.load_calibration_params(self.__bus, self.__address)
         self.__rain_sensor = Button(6)
+        self.__wind_speed_sensor = Button(5)
+        self.__wind_count = 0
+        self.__wind_direction_sensor = MCP3008(channel=0)
+        self.__wind_dir_data = []
 
     def __get_bme280(self):
         """
@@ -48,7 +53,7 @@ class Sensors():
         self.__logger.log("info", "Recording detected rainfall")
         now_str = datetime_string()
 
-        with open("rainfall.txt", "a") as rainfile:
+        with open("rain.txt", "a") as rainfile:
             rainfile.write(now_str + "\n")
 
     def setup_rain_sensor(self):
@@ -66,7 +71,7 @@ class Sensors():
         Returns:
             int: amount of rainfall in mm
         """
-        if not path.isfile("rain.txt"):
+        if not os.path.isfile("rain.txt"):
             return 0
 
         rain_mm = 0
@@ -78,10 +83,154 @@ class Sensors():
             if entry:
                 rain_mm += RAIN_SENSOR_MM
 
-        remove("rain.txt")
+        os.remove("rain.txt")
 
         return rain_mm
     
+    def __spin(self):
+        """
+        Record a single half rotation of the anemometer as well as wind direction
+        
+        Note:
+            It's a half rotation due to the anemometer generating two signals
+            per whole rotation
+        """
+        self.__wind_count += 1
+        self.__record_wind_direction()
+
+    def setup_anemometer(self):
+        """
+        Sets up the listener for the anemometer
+        """
+        self.__wind_speed_sensor.when_activated = self.__spin
+        self.__logger.log("info", "- Anemometer sensor setup complete")
+
+    def record_wind_data(self):
+        """
+        Calculates wind speed from wind_count and WIND_INTERVAL and
+        average wind direction and stores to file
+        """
+        circumference_cm = (2 * math.pi) * WIND_RADIUS
+        rotations = self.__wind_count / 2.0
+
+        # Distance travelled by an anemometer cup in cm
+        dist_cm = circumference_cm * rotations
+
+        # Wind speed, convert to m/s and adjust for anemometer factor
+        speed = ((dist_cm / WIND_INTERVAL) / 100) * WIND_ADJUSTMENT
+
+        # Finally get average wind direction
+        average_wind_dir = 0.0
+        if len(self.__wind_dir_data) > 0:
+            average_wind_dir = self.__get_average_wind_dir()
+        elif os.path.isfile("wind_dir.txt"):
+            # If there's not been any wind, duplicate the most recent dir since
+            # the vane won't have moved
+            with open("wind_dir.txt", "r") as read_dirfile:
+                last_dir = read_dirfile.readlines()[-1].rstrip()
+        # If there's no wind or previous dir, just leave it as 0.0. Should probably
+        # look into always storing most recent dir for this
+
+        # Save to file
+        with open("wind_speed.txt", "a") as speedfile:
+            speedfile.write(str(speed) + "\n")
+        
+        with open("wind_dir.txt", "a") as dirfile:
+            dirfile.write(str(average_wind_dir) + "\n")
+
+        self.__wind_count = 0
+        self.__wind_dir_data = []
+
+    def __get_average_wind_dir(self, data=[]):
+        """
+        Get average wind direction from list of readings. Maths shamelessly stolen from
+        the internet because I'm a dev, not a shape wizard
+
+        Returns:
+            int: angle of average wind direction
+        """
+        if len(data) == 0:
+            data = self.__wind_dir_data
+
+        sin_sum = 0.0
+        cos_sum = 0.0
+
+        for angle in data:
+            r = math.radians(angle)
+            sin_sum += math.sin(r)
+            cos_sum += math.cos(r)
+
+        f_len = float(len(data))
+        s = sin_sum / f_len
+        c = cos_sum / f_len
+        arc = math.degrees(math.atan(s / c))
+        average = 0.0
+
+        if s > 0 and c > 0:
+            average = arc
+        elif c < 0:
+            average = arc + 180
+        elif s < 0 and c > 0:
+            average = arc + 360
+
+        return 0.0 if average == 360 else average
+
+    def __record_wind_direction(self):
+        """
+        Record current wind direction based on voltage reading from vane to memory
+        """
+        wind_dir = round(self.__wind_direction_sensor.value*3.3,1)
+        # TODO: On bad readings, find closest angle and save that instead
+        if wind_dir in WIND_DIR_VOLTS:
+            self.__wind_dir_data.append(WIND_DIR_VOLTS[wind_dir])
+
+    def __get_wind_speed_data(self):
+        """
+        Calculate average wind speed and gust speed from currently saved readings 
+
+        Returns:
+            float: average wind speed in m/s
+            float: gust speed in m/s
+        """
+        wind_speeds = []
+        with open("wind_speed.txt", "r") as speedfile:
+            speeds = speedfile.readlines()
+            for speed in speeds:
+                f_speed = float(speed.rstrip())
+                wind_speeds.append(f_speed)
+
+        gust = max(wind_speeds)
+        avg_speed = statistics.mean(wind_speeds)
+
+        os.remove("wind_speed.txt")
+
+        return gust, avg_speed
+
+    def __get_wind_dir_data(self):
+        """
+        Calculate average wind direction from currently saved readings
+
+        Returns:
+            float: average angle of wind direction
+        """
+        wind_dirs = []
+        with open("wind_dir.txt", "r") as dirfile:
+            dirs = dirfile.readlines()
+            for dir in dirs:
+                f_dir = float(dir.rstrip())
+                wind_dirs.append(f_dir)
+        
+        average_dir = self.__get_average_wind_dir(wind_dirs)
+        
+        # Return closest cardinal/ordinal angle
+        compass_angles = [0, 45, 90, 135, 180, 225, 270, 315, 360]
+        closest_angle = min(compass_angles, key=lambda x: abs(x - average_dir))
+
+        os.remove("wind_dir.txt")
+
+        return 0 if closest_angle == 360 else closest_angle
+            
+        
     def get_readings(self):
         """
         Take readings from all sensors and return a dict containing them
@@ -93,15 +242,18 @@ class Sensors():
 
         bme280_readings = self.__get_bme280()
         rainfall = self.__get_rainfall()
+        gust, avg_speed = self.__get_wind_speed_data()
+        wind_dir = self.__get_wind_dir_data()
         readings_dict = {
                 "temperature": round(bme280_readings["temperature"], 2),
                 "humidity": round(bme280_readings["humidity"], 2),
                 "pressure": round(bme280_readings["pressure"], 2),
                 "luminance": 0,
-                "wind_speed": 0,
+                "wind_speed": avg_speed,
+                "gust_speed": gust,
+                "wind_direction": wind_dir,
                 "rain": rainfall,
                 "rain_per_second": 0,
-                "wind_direction": 0
         }
 
         return readings_dict
--- config_template.py
+++ config_template.py
@@ -1,3 +1,4 @@
+# Software constants
 # Name of the station to identify it in the readings database
 NICKNAME=""
 # Increment on the hour to take readings. e.g. 15 means readings at 0, 15, 30, and 45, minutes
@@ -9,5 +10,30 @@ UPLOAD_FREQUENCY=4
 UPLOAD_DESTINATION=""
 # Format for string representations of timestamps
 TIME_FORMAT=""
+# How often to calculate and record wind speed in seconds
+WIND_INTERVAL=5
+# Adjustment for anemometer factor
+WIND_ADJUSTMENT=1.1789
+# Dict for looking up wind direction angle based on wind vane voltage
+WIND_DIR_VOLTS = {0.4: 0.0,
+                   1.4: 22.5,
+                   1.2: 45.0,
+                   2.8: 67.5,
+                   2.7: 90.0,
+                   2.9: 112.5,
+                   2.2: 135.0,
+                   2.5: 157.5,
+                   1.8: 180.0,
+                   2.0: 202.5,
+                   0.7: 225.0,
+                   0.8: 247.5,
+                   0.1: 270.0,
+                   0.3: 292.5,
+                   0.2: 315.0,
+                   0.6: 337.5}
+
+# Hardware constants
 # Amount of rain required for the rain sensor to trip in mm
 RAIN_SENSOR_MM = 0.2794
+# Radius of anemometer in cm
+WIND_RADIUS = 9.0
