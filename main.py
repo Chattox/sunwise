76bc05d023332ffbea11760d0e2da37588450fdb
Chattox <conradlineker@gmail.com>
2025-03-22 12:23:48 +0000

    feat: exception logging

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from traceback import format_exc
 from sunwise.Sunwise import Sunwise
+from utils import datetime_string
 
-sunwise = Sunwise()
-sunwise.watch_weather()
+try:
+    sunwise = Sunwise()
+    sunwise.watch_weather()
+except Exception as e:
+    now = datetime_string()
+
+    log_entry = "{0} {1:<10} {2}".format(now, "[exception]: ", format_exc())
+
+    print(log_entry)
+
+    with open("log.txt", "a") as logfile:
+        logfile.write(log_entry + "\n")
